ee9e5950295aef9063cdeee0b585b09311b4e588
Ozan <ozancangu24@gmail.com>
2023-07-20 16:13:13 +0300

    -Customer Dao created

---
+++ app/src/main/java/com/ozancanguz/customercontacts/data/db/CustomerDao.kt
@@ -0,0 +1,18 @@
+package com.ozancanguz.customercontacts.data.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Insert
+import androidx.room.Query
+
+interface CustomerDao {
+
+
+
+
+    @Query("select*from customer_table order by id asc")
+    fun getAllCustomerContact():LiveData<List<Customer>>
+
+    @Insert
+    suspend fun insertCustomer(customer: Customer)
+
+}
